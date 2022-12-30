6848326b257b72e9dc2ad22285b40759bdcd7847
dedyzhang <dedyzhang14@gmail.com>
2022-12-30 11:46:42 +0700

    Initial express server with route File

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dedyzhang:bodohamat0@cluster0.69kqbtu.mongodb.net/test",
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose"); //#2
+
+const user = require("./routes/api/user"); //#3
+const post = require("./routes/api/post"); //#3
+const profile = require("./routes/api/profile"); //#3
+
+const app = express();
+
+// DB Config
+const db = require("./config/key").mongoURI; //#2
+
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("success connectfully"))
+  .catch((e) => console.log(e)); //#2
+
+app.get("/", (req, res) => res.send("Hello!"));
+
+app.use("/api/users", user); //#3
+app.use("/api/profile", profile); //#3
+app.use("/api/post", post); //#3
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on PORT ${port}`));
