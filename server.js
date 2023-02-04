e7cab502a55a51ac7dd6810907456346d48255e5
dedyzhang <dedyzhang14@gmail.com>
2023-02-04 10:54:31 +0700

    'IniAdalahUpdateRegistration'

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose"); //#2
+const bodyParser = require('body-parser'); //#5
 
 const user = require("./routes/api/user"); //#3
 const post = require("./routes/api/post"); //#3
@@ -7,12 +8,15 @@ const profile = require("./routes/api/profile"); //#3
 
 const app = express();
 
+//Body parser middleware #5
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
 // DB Config
 const db = require("./config/key").mongoURI; //#2
 
 //connect to mongoDB
-mongoose
-  .connect(db)
+mongoose.connect(db)
   .then(() => console.log("success connectfully"))
   .catch((e) => console.log(e)); //#2
 
